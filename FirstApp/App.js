dfce4e0801271f32121d53818011e8f88c9dd946
Samuel <s.yun@arcane.ws>
2018-02-23 16:39:42 -0500

    Added a limiter to the budget and a compare function that will tell you when you're over. Disable button functionality next

--- FirstApp/App.js
+++ FirstApp/App.js
@@ -40,6 +40,7 @@ export default class FirstApp extends Component {
             })
         };
         this.itemsRef = this.getRef().child('items');
+        this.limitRef = this.getRef().child('limit');
         console.ignoredYellowBox = ['Remote debugger'];
         console.ignoredYellowBox = ['FIREBASE WARNING: Invalid query string segment:'];
     }
@@ -48,7 +49,7 @@ export default class FirstApp extends Component {
         return firebaseApp.database().ref();
     }
 
-    listenForItems(itemsRef) {
+    listenForItems(itemsRef, limitRef) {
         itemsRef.on('value', (snap) => {
 
             // get children as an array
@@ -67,10 +68,26 @@ export default class FirstApp extends Component {
             });
 
         });
+        limitRef.on('value', (snap) => {
+
+            // get children as an array
+            var items = [];
+            snap.forEach((child) => {
+                items.push({
+                    limit: child.val()
+                });
+            });
+
+            this.setState({
+                limitExpense: this.limitExpense(items)
+            });
+
+        });
+
     }
 
     componentDidMount() {
-        this.listenForItems(this.itemsRef);
+        this.listenForItems(this.itemsRef, this.limitRef);
     }
 
     render() {
@@ -78,7 +95,9 @@ export default class FirstApp extends Component {
             <View style={styles.container}>
 
                 <StatusBar title="Expenses - Tap Items for Actions" />
-                <StatusBar title={this.state.totalExpense} />
+                {this.state.totalExpense < this.state.limitExpense ? <Text style={styles.limitSafe}>${this.state.totalExpense} out of ${this.state.limitExpense}</Text>: null}
+                {this.state.totalExpense == this.state.limitExpense ? <Text style={styles.limitWarning}>${this.state.totalExpense} out of ${this.state.limitExpense}</Text>: null}
+                {this.state.totalExpense > this.state.limitExpense ? <Text style={styles.limitDanger}>${this.state.totalExpense} out of ${this.state.limitExpense}</Text>: null}
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={this._renderItem.bind(this)}
@@ -86,6 +105,7 @@ export default class FirstApp extends Component {
                     style={styles.listview}/>
 
                 <ActionButton onPress={this._addItem.bind(this)} title="Add New Item" />
+                <ActionButton onPress={this._addLimit.bind(this)} title="Edit Limit" />
 
             </View>
 
@@ -93,17 +113,27 @@ export default class FirstApp extends Component {
     }
 
     totalExpense(items) {
-        let sign = "$";
         let total = 0;
         items.forEach(function(element) {
-            total += parseInt(element.value);
+            if(element.value) {
+                total += parseInt(element.value);
+            }
         });
         if(total > 0) {
-            return sign.concat(total.toString());
+            return total;
         }
         else {
-            return "$0";
+            return 0;
         }
+    }
+
+    limitExpense(items) {
+        if(items[0].limit > 0) {
+            return items[0].limit;
+        }
+        else {
+            return 0;
+        };
     }
 
     _addItem() {
@@ -116,6 +146,23 @@ export default class FirstApp extends Component {
                     text: 'Add',
                     onPress: (text) => {
                         this.itemsRef.push({ title: text })
+                    }
+                },
+            ],
+            'plain-text'
+        );
+    }
+
+    _addLimit() {
+        AlertIOS.prompt(
+            'Place a Limit',
+            null,
+            [
+                {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                {
+                    text: 'Update',
+                    onPress: (text) => {
+                        this.limitRef.update({ limit: text })
                     }
                 },
             ],
